146604e5e40a474d99cb816cb05fa7c793403944
Aleksey29247 <95174048+Aleksey29247@users.noreply.github.com>
2022-03-23 20:37:12 +0700

    Update CashbackHackServiceTest.java

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -18,14 +18,14 @@ public class CashbackHackServiceTest {
     public void testMore1000() {
         int amount = 10000;
         int actual = cashbackHackService.remain(amount);
-        int expected = 1000;
+        int expected = 0;
         assertEquals(expected, actual);
     }
     @Test
     public void testEquals1000() {
         int amount = 1000;
         int actual = cashbackHackService.remain(amount);
-        int expected = 1000;
+        int expected = 0;
         assertEquals(expected, actual);
    }
 
