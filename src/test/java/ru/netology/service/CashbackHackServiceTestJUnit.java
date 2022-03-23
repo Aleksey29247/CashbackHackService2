97f9452777a5526c76bced6e466f02e47a3983e0
Aleksey <Alik29247@gmail.com>
2022-03-23 20:26:11 +0700

    TUnit4

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTestJUnit.java
@@ -0,0 +1,34 @@
+package ru.netology.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CashbackHackServiceTestJUnit {
+    CashbackHackService cashbackHackService = new CashbackHackService();
+
+    @Test
+    public void test1() {
+        int amount = 900;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 100;
+        assertEquals( expected,actual);
+    }
+
+   @Test
+    public void testMore1000() {
+        int amount = 10000;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 1000;
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void testEquals1000() {
+        int amount = 1000;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 1000;
+        assertEquals(expected, actual);
+    }
+
+
+}
